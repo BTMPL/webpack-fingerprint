05ce0537c3c72fba2fe19113d664b94a190e8fce
Bartosz Szczecinski <baphemot@gmail.com>
2018-02-06 13:48:00 +0100

    Added support for Webpack 1.x

--- index.js
+++ index.js
@@ -11,34 +11,60 @@ function WebpackFingerprint(opts) {
       license: package.license
     })
   }, opts);
-  
+
 }
 
-WebpackFingerprint.prototype.apply = function(compiler) {
+WebpackFingerprint.prototype.apply = function (compiler) {
   var self = this;
 
-  compiler.plugin('done', function(data) {
+  compiler.plugin('done', function (data) {
     const stats = Object.assign({}, {
       date: new Date(),
       version: '',
     }, self.opts.additional, { packages: {} });
 
-    const projectPackageJson = path.resolve(path.join('./', 'package.json'));    
-    if(fs.existsSync(projectPackageJson)) {
+    const projectPackageJson = path.resolve(path.join('./', 'package.json'));
+    if (fs.existsSync(projectPackageJson)) {
       const package = require(projectPackageJson);
       stats.version = package.version
     }
 
     Object.values(data.compilation._modules).forEach(module => {
       const modulePath = module.portableId;
-      if(modulePath.indexOf('node_modules') === 0) {      
+      
+      // Initial support for webpack 1.x syntax
+      if (!modulePath && Array.isArray(module.dependencies)) {
+        module.dependencies.forEach(dependency => {
+          if (!dependency.module) return;
+          if (dependency.module.context && dependency.module.context.indexOf('node_modules') !== -1) {
+            const modulePath = path.parse(dependency.module.context.split('node_modules' + path.sep)[1]);
+            
+            const [moduleName, ...rest] = (modulePath.dir !== '' ? modulePath.dir : modulePath.base).split(path.sep);
+            if (typeof stats.packages[moduleName] !== 'undefined') return;
+            const packageJson = path.join(moduleName, 'package.json');
+            if (fs.existsSync(path.join('node_modules', moduleName)) && fs.existsSync(path.join('node_modules', packageJson))) {
+              const package = require(packageJson);
+              const result = self.opts.transformer(package);
+              if (result) stats.packages[moduleName] = result;
+            }
+            else {
+              stats.packages[moduleName] = {
+                version: 'unknown',
+                license: 'unknown'
+              }
+            }
+          }
+        })
+      }
+
+      if (modulePath && modulePath.indexOf('node_modules') === 0) {
         const [_, moduleName, ...rest] = path.parse(modulePath).dir.split('/');
-        if(typeof stats.packages[moduleName] !== 'undefined') return;
+        if (typeof stats.packages[moduleName] !== 'undefined') return;
         const packageJson = path.join(moduleName, 'package.json');
-        if(fs.existsSync(path.join('node_modules', packageJson))) {
+        if (fs.existsSync(path.join('node_modules', packageJson))) {
           const package = require(packageJson);
           const result = self.opts.transformer(package);
-          if(result) stats.packages[moduleName] = result;
+          if (result) stats.packages[moduleName] = result;
         }
         else {
           stats.packages[moduleName] = {
@@ -48,11 +74,11 @@ WebpackFingerprint.prototype.apply = function(compiler) {
         }
       }
     })
-    if(Object.values(stats.packages).length === 0) {
+    if (Object.values(stats.packages).length === 0) {
       delete stats.packages;
     }
-    fs.writeFile(self.opts.filename, JSON.stringify(stats, null, 2), function(err) {
-      if(err) console.warn("Unable to write fingerprint file", err);
+    fs.writeFileSync(self.opts.filename, JSON.stringify(stats, null, 2), function (err) {
+      if (err) console.warn("Unable to write fingerprint file", err);
     })
   });
 };
